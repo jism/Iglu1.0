3df2ec82b43746413d68f1bf71f64d5dac10356e
jism <jism@ciencias.unam.mx>
2015-04-09 14:58:56 -0500

    Actualizacion

--- Videojuegos/src/java/Controlador/IniciarSesion.java
+++ Videojuegos/src/java/Controlador/IniciarSesion.java
@@ -1,3 +1,5 @@
+package Controlador;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -57,7 +59,8 @@ public class IniciarSesion extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
+        response.sendRedirect("Administrador.jsp");
     }
 
     /**
